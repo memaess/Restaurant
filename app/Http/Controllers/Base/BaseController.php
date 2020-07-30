48ae7b1a734fd4fca0a78c4beb1a96d70f08b328
Nazoru002 <nanozero123@gmail.com>
2020-07-30 20:24:47 +0700

    second commit

---
+++ app/Http/Controllers/Base/BaseController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Base;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{
+    public function index()
+    {
+        return view('welcome');
+    }
+}
